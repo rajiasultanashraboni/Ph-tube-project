9197537e82cf462af697cb8bfb024d2f590517c6
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-10-05 12:17:48 +0600

    button sections are dynamically completed here

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("script added here");
+
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+  categories.forEach((item) => {
+    console.log(item);
+
+    // create button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+
+    // add button to category container
+    categoryContainer.append(button);
+  });
+};
+
+loadCategories();
